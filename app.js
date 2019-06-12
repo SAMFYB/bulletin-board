2e5b970bd5d03b6d89b6d7c3613b3ad39faa27fc
Sam Yong <yongmc.sam2014@outlook.com>
2019-06-11 21:54:54 -0400

    get data

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+const container = document.querySelector("#onscreen-container");
+
+// init container styles
+container.style.height = window.innerHeight + "px";
+container.style.width = window.innerWidth + "px";
+container.style.position = "fixed";
+container.style.top = 0;
+container.style.left = 0;
+
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     const uid = user.uid;
     console.log(uid);
+    const userDocId = await getUserDocId(uid);
+    console.log(userDocId);
+    const userData = await getUserData(userDocId);
+    const onscreen = userData.onscreen;
+    console.log(onscreen);
+    const screenData = await getScreenData(userDocId, onscreen);
+    container.style.backgroundColor = screenData.colorbg;
+    const cardsAndItems = await getScreenCardsAndItems(userDocId, onscreen);
   } else {
     console.log("User Not Signed In");
   }
