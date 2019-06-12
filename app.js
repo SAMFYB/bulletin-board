47d4acde2f32686b215047d685e8da517a10ef08
Sam Yong <yongmc.sam2014@outlook.com>
2019-06-11 21:14:43 -0400

    store

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-const db = firebase.firestore();
-
-// persist signed-in user ID and Firebase user document ID
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     const uid = user.uid;
-    setCookieUser(uid);
-    const userDocRef = db.collection("users").where("uid", "==", uid);
-    const snapshot = await userDocRef.get();
-    snapshot.forEach(doc => {
-      setCookieUserDocId(doc.id);
-    });
+    console.log(uid);
   } else {
-    eraseCookie();
+    console.log("User Not Signed In");
   }
 });
---
+++ store.js
@@ -0,0 +1,53 @@
+const db = firebase.firestore();
+
+async function getUserDocId(uid) {
+  const snapshot = await db.collection("users").where("uid", "==", uid).get();
+  let docId;
+  snapshot.forEach(doc => {
+    docId = doc.id;
+  });
+  return docId;
+}
+
+async function getUserData(userDocId) {
+  const snapshot = await db.collection("users").doc(userDocId).get();
+  return snapshot.data();
+}
+
+async function getUserScreens(userDocId) {
+  const snapshot = await db.collection("users").doc(userDocId).collection("screens").get();
+  let screens = [];
+  snapshot.forEach(doc => {
+    screens.push(doc.id);
+  });
+  return screens;
+}
+
+async function getScreenData(userDocId, screenId) {
+  const snapshot = await db.collection("users").doc(userDocId).collection("screens").doc(screenId).get();
+  return snapshot.data();
+}
+
+async function getScreenCardsAndItems(userDocId, screenId) {
+  const cardsRef = db.collection("users").doc(userDocId).collection("screens").doc(screenId).collection("cards");
+  const snapshot = await cardsRef.get();
+  let cards = [];
+  snapshot.forEach(cardDoc => {
+    const cardId = cardDoc.id;
+    const cardData = cardDoc.data();
+    const itemsSnapshot = await cardsRef.doc(cardId).collection("items");
+    let items = [];
+    itemsSnapshot.forEach(itemDoc => {
+      items.push({
+        itemId: itemDoc.id,
+        itemData: itemDoc.data()
+      });
+    });
+    cards.push({
+      cardId,
+      cardData,
+      items
+    });
+  });
+  return cards;
+}
